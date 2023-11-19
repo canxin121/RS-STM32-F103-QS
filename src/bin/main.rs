bf77644ed6c439c478e4f5ac86d52ee55f7e8218
canxin <1969730106@qq.com>
2023-11-20 00:21:41 +0800

    init

---
+++ src/bin/main.rs
@@ -0,0 +1,44 @@
+#![no_std]
+#![no_main]
+
+use core::fmt::Write;
+
+use cortex_m_rt::entry;
+use panic_halt as _;
+use stm32f1xx_hal::{
+    pac,
+    prelude::*,
+    serial::{Config, Serial},
+};
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let rcc = dp.RCC.constrain();
+    let mut afio = dp.AFIO.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let mut gpioa = dp.GPIOA.split();
+
+    let serial = Serial::new(
+        dp.USART1,
+        (
+            gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh),
+            gpioa.pa10,
+        ),
+        &mut afio.mapr,
+        Config::default().baudrate(115200.bps()),
+        &clocks,
+    );
+    let (mut tx, mut _rx) = serial.split();
+
+    let mut led = gpioa.pa0.into_push_pull_output(&mut gpioa.crl);
+    let mut delay = cp.SYST.delay(&clocks);
+    loop {
+        writeln!(tx, "toggle.").unwrap();
+        led.toggle();
+        delay.delay_ms(1000u16);
+    }
+}
