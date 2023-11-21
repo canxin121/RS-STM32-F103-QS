fd8c14c2a041309e58a04cb8f4f0b99f8c5d1599
canxin <1969730106@qq.com>
2023-11-21 16:21:47 +0800

    update

--- defmt_print.bat
+++ defmt_print.bat
@@ -2,10 +2,10 @@
 setlocal enabledelayedexpansion
 set /a count=0
 :loop
-C:/Users/Administrator/.cargo/bin/defmt-print.exe -e ./target/thumbv7m-none-eabi/debug/%~1 tcp --port 6666
+defmt-print -e %~1 tcp --port 6666
 if %errorlevel% neq 0 (
     set /a count+=1
-    echo Command failed, retrying in 1 seconds
+    echo Failed to connect to OpenOCD RTT tcp server, retrying in 2 seconds
     timeout /t 2 /nobreak
     cls
     goto loop
